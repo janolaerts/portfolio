cf5c0f4b05d3068be871e81ab16064dcd5617773
iamjanolaerts <jan.olaerts92@gmail.com>
2019-11-08 17:29:14 -0500

    Before adding Navbar

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -4,6 +4,19 @@
       <h1>Hello, I am Jan and I build</h1>
       <h1 :class="`words ${this.word}`">{{ word }}</h1>
     </div>
+    <div class="technologies">
+      <h1>I use <span>{{ technology }}</span></h1>
+      <div class="logos">
+        <img @mouseover="showTech('HTML')" height="32" width="32" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/html5.svg" />
+        <img @mouseover="showTech('CSS')" height="32" width="32" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/css3.svg" />
+        <img @mouseover="showTech('JavaScript')" height="32" width="32" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/javascript.svg" />
+        <img @mouseover="showTech('React')" height="32" width="32" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/react.svg" />
+        <img @mouseover="showTech('Node.js')" height="32" width="32" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/node-dot-js.svg" />
+        <img @mouseover="showTech('Vue.js')" height="32" width="32" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/vue-dot-js.svg" />
+        <img @mouseover="showTech('Firebase (db & auth)')" height="32" width="32" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/firebase.svg" />
+        <img @mouseover="showTech('MongoDB')" height="32" width="32" src="https://cdn.jsdelivr.net/npm/simple-icons@latest/icons/mongodb.svg" />
+      </div>
+    </div>
     <div class="links">
       <div @click="$router.push({ name: 'Projects' })"><img src="../assets/code.svg" alt="project"><h2 class="link-my-projects">My projects</h2></div>
       <div @click="$router.push({ name: 'Resume' })"><img src="../assets/text-file-line.svg" alt="resume"><h2 class="link-resume">Resume</h2></div>
@@ -12,7 +25,7 @@
     <div class="contact">
       <button @click="$router.push({ name: 'Contact' })">Contact me</button>
     </div>
-      <img class="arrow" src="../assets/arrow.svg" alt="menu">
+      <img class="arrow" src="../assets/arrow.svg" alt="contact">
   </div>
 </template>
 
@@ -26,7 +39,8 @@ export default {
      words: ['applications', 'websites', 'market places', 'native apps'],
      index: 0,
      button: false,
-     github: 'https://github.com/janolaerts'
+     technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Vue.js', 'Firebase (db & auth)', 'MongoDB'],
+     technology: null
     }
   },
   created(){
@@ -41,6 +55,9 @@ export default {
       if(this.index >= 4){
         this.index = 0
       }
+    },
+    showTech(technology){
+      this.technology = technology
     }
   }
 }
@@ -58,9 +75,7 @@ export default {
 .intro h1{
   position: relative;
   color: white;
-  font-size: 300%;
   padding: 0px 20px;
-  top: 100px;
   font-size: 50px;
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   margin: 0;
@@ -81,13 +96,34 @@ export default {
   position: relative;
   left: 0px;
 }
+.intro .technologies{
+  position: absolute;
+  width: 100%;
+  height: 100px;
+  bottom: 140px;
+  border-top: 2px solid #cd6677;
+  border-bottom: 2px solid #cd6677;
+}
+.intro .technologies h1{
+  position: relative;
+  font-size: 40px;
+  text-align: center;
+}
+.intro .technologies .logos{
+  position: relative;
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  top: 20%;
+}
+.intro .technologies .logos img{
+  margin: 0 auto;
+}
 .intro .links{
   position: absolute;
   height: 25%;
   width: 45px;
-  bottom: 30%;
+  bottom: 300px;
   right: 10px;
-
 }
 .intro .links div, .intro .links a{
   position: absolute;
@@ -105,18 +141,12 @@ export default {
 }
 .intro .links div:nth-child(1){
   margin-top: 0px;
-  animation: fall1 2s linear;
-  animation-timing-function: ease;
 }
 .intro .links div:nth-child(2){
   margin: 75px 0;
-  animation: fall2 1.5s linear;
-  animation-timing-function: ease;
 }
 .intro .links a{
   margin: 150px 0;
-  animation: fall3 1s linear;
-  animation-timing-function: ease;
 }
 .intro .links div:nth-child(1):hover{
   background: #cd6689;
@@ -142,15 +172,7 @@ export default {
   right: 60px;
   width: 150px;
   cursor: pointer;
-}
-.intro .links h2.link-my-projects{
-  bottom: 462px;
-}
-.intro .links h2.link-resume{
-  bottom: 387px;
-}
-.intro .links h2.link-github{
-  bottom: 312px;
+  margin-top: 6px;
 }
 .intro .contact{
   position: absolute;
@@ -193,30 +215,7 @@ export default {
     transform: translateY(-15px);
   }
 }
-/*@keyframes fall1{
-  from{
-    top: -300%
-  }
-  to{
-    top: 0%
-  }
-}
-@keyframes fall2{
-  from{
-    top: -300%
-  }
-  to{
-    top: 0%
-  }
-}
-@keyframes fall3{
-  from{
-    top: -300%
-  }
-  to{
-    top: 0%
-  }
-}*/
+
 @keyframes menu1{
   from{
     width: 0px
@@ -239,6 +238,34 @@ export default {
   }
   to{
     width: 210px
+  }
+}
+
+/* responsive rules */
+@media all and (max-width: 991px){
+  .intro h1{
+    font-size: 40px;
+  }
+  .intro .technologies h1{
+    font-size: 35px;
+  }
+}
+
+@media all and (max-width: 768px){
+  .intro h1{
+    font-size: 35px;
+  }
+  .intro .technologies h1{
+    font-size: 30px;
+  }
+}
+
+@media all and (max-width: 575px){
+  .intro h1{
+    font-size: 30px;
+  }
+  .intro .technologies h1{
+    font-size: 25px;
   }
 }
 
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>Projects</h1>
+  <div class="projects">
+    <h1><span>M</span><span>y</span><span>-</span><span>p</span><span>r</span><span>o</span><span>j</span><span>e</span><span>c</span><span>t</span><span>s</span></h1>
+    <div class="flex-container">
+      <div class="project1">
+        <h2>Jatha Ventas</h2>
+      </div>
+      <div class="project2">
+        <h2>Taco App</h2>
+      </div>
+      <div class="project3">
+        <h2>Askbot</h2>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,12 +20,131 @@ export default {
   name: 'Projects',
   data(){
     return{
-
+      wiggle: false
     }
   }
 }
 </script>
 
 <style>
+.projects{
+  background: #cd9166;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+.projects h1{
+  position: fixed;
+  color: #cd6677;
+  font-size: 300%;
+  padding: 20px;
+  font-size: 50px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  margin: 0;
+  padding: 0px 0px 0px 2.5%;
+  z-index: 1000;
+  background: rgba(205, 145, 102, .5);
+  width: 100%;
+}
+.projects h1 span{
+  position: relative;
+  animation-timing-function: ease;
+}
+.projects h1 span:nth-child(1){
+  animation: slide 1.5s linear forwards;
+}
+.projects h1 span:nth-child(2){
+  animation: slide 1.4s linear forwards;
+}
+.projects h1 span:nth-child(3){
+  visibility: hidden;
+  animation: slide 1.3s linear forwards;
+}
+.projects h1 span:nth-child(4){
+  animation: slide 1.2s linear forwards;
+}
+.projects h1 span:nth-child(5){
+  animation: slide 1.1s linear forwards;
+}
+.projects h1 span:nth-child(6){
+  animation: slide 1s linear forwards;
+}
+.projects h1 span:nth-child(7){
+  animation: slide .9s linear forwards;
+}
+.projects h1 span:nth-child(8){
+  animation: slide .8s linear forwards;
+}
+.projects h1 span:nth-child(9){
+  animation: slide .7s linear forwards;
+}
+.projects h1 span:nth-child(10){
+  animation: slide .6s linear forwards;
+}
+.projects h1 span:nth-child(11){
+  animation: slide .5s linear forwards;
+}
+.flex-container{
+  position: relative;
+  top: 75px;
+  width: 95%;
+  height: 650px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 5px;
+}
+.flex-container .project1{
+  background: url('../assets/jatha-ventas.png') no-repeat top;
+  background-size:cover;
+  flex-basis: 450px;
+  flex-grow: 1;
+  margin: 5px;
+  border-radius: 10px;
+}
+.flex-container .project2{
+  background: url('../assets/taco-app.png') no-repeat top;
+  background-size: cover;
+  flex-basis: 450px;
+  flex-grow: 1;
+  margin: 5px;
+  border-radius: 10px;
+}
+.flex-container .project3{
+  background: url('../assets/askbot.png') no-repeat top;
+  background-size: cover;
+  flex-basis: 450px;
+  flex-grow: 1;
+  margin: 5px;
+  border-radius: 10px;
+}
 
+/* animations */
+@keyframes slide{
+  from{
+    left: -300px;
+  }
+  to{
+    left: 0px;
+  }
+}
+@keyframes wiggle{
+  0%{
+    left: -10px;
+  }
+  25%{
+    left: 10px;
+  }
+  50%{
+    left: -5px;
+  }
+  75%{
+    left: 5px;
+  }
+  100%{
+    left: 0px;
+  }
+}
 </style>
